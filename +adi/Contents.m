dcc06915c2ece9d2192867101daf503b7bcc8e36
Stanca Pop <stanca.pop@analog.com>
2023-06-26 17:55:03 +0300

    Add cn0585_fmcz support

--- +adi/Contents.m
+++ +adi/Contents.m
@@ -9,5 +9,12 @@
 %   <a href="matlab:help adi.AD7768            ">AD7768</a>         - ADC
 %   <a href="matlab:help adi.AD7768_1          ">AD7768-1</a>       - ADC
 %   <a href="matlab:help adi.AD4030            ">AD4030-24</a>      - ADC
-%   <a href="matlab:help adi.AD4630_16          ">AD4630-16</a>     - ADC
-%   <a href="matlab:help adi.AD4630_24          ">AD4630-24</a>     - ADC
+%   <a href="matlab:help adi.AD4630_16         ">AD4630-16</a>      - ADC
+%   <a href="matlab:help adi.AD4630_24         ">AD4630-24</a>      - ADC
+%   <a href="matlab:help adi.AD3552R           ">AD43552R</a>       - DAC
+%   <a href="matlab:help adi.LTC2387           ">LTC2387</a>        - DAC
+%
+% Boards and Platforms
+% -----------------------
+%   <a href="matlab:help adi.CN0585            ">CN0585</a>         - FMC development board for precision data acquisition
+
